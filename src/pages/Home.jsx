2e9337faead3f9bc4d4da553b2cf42245a683f71
Muhammadalimran22 <126676596+Muhammadalimran22@users.noreply.github.com>
2023-04-05 19:44:08 +0700

    feature: navbar (#1)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Container } from "react-bootstrap";
+
+function Home() {
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
+}
+
+export default Home;
