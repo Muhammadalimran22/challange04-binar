f22ba9f91f95152ef4899c484c554c74e9cf5d29
msyahdana <msyahdanabdillah@gmail.com>
2023-04-10 22:03:46 +0700

    feature: SearchPage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import HomeCard from "./components/HomeCard";
-
 import "bootstrap/dist/css/bootstrap.min.css";
+import SearchedMovie from "./pages/SearchedMovie";
+import DetailMovie from "./pages/DetailMovie";
 function App() {
   return (
     <BrowserRouter>
-      <HomeCard />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchedMovie />} />
+        <Route path="/detail/:id" element={<DetailMovie />} />
       </Routes>
     </BrowserRouter>
   );
